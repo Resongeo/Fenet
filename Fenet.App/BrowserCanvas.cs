671adbeb6a395235178ce0446fe007fb54537b60
Resongeo <s.benedek2003@gmail.com>
2025-06-21 13:06:17 +0200

    feat(app): Test Skia drawing

---
+++ Fenet.App/BrowserCanvas.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Platform;
+using Avalonia.Rendering.SceneGraph;
+using Avalonia.Skia;
+using Fenet.Core;
+using SkiaSharp;
+
+namespace Fenet.App;
+
+public class BrowserCanvas : Control
+{
+    private readonly BrowserEngine _browserEngine = new();
+    private readonly GlyphRun _noSkiaFallback;
+
+    public BrowserCanvas()
+    {
+        ClipToBounds = true;
+        const string noSkiaText = "Skia is not available";
+        var glyphs = noSkiaText
+            .Select(ch => Typeface.Default.GlyphTypeface.GetGlyph(ch))
+            .ToArray();
+        _noSkiaFallback = new GlyphRun(Typeface.Default.GlyphTypeface, 12, noSkiaText.AsMemory(), glyphs);
+    }
+    
+    public void Navigate(string url)
+    {
+        // TODO: download source from url
+        _browserEngine.TestParse(url);
+    }
+
+    public override void Render(DrawingContext context)
+    {
+        var bounds = new Rect(0, 0, Bounds.Width, Bounds.Height);
+        context.Custom(new CustomDrawOp(bounds, _noSkiaFallback));
+    }
+}
+
+internal class CustomDrawOp(Rect bounds, GlyphRun noSkiaFallback) : ICustomDrawOperation
+{
+    public Rect Bounds { get; } = bounds;
+
+    private readonly IImmutableGlyphRunReference? _noSkiaFallback
+        = noSkiaFallback.TryCreateImmutableGlyphRunReference();
+
+    public void Render(ImmediateDrawingContext context)
+    {
+        var leaseFeature = context.TryGetFeature<ISkiaSharpApiLeaseFeature>();
+
+        if (leaseFeature == null)
+        {
+            context.DrawGlyphRun(Brushes.Black, _noSkiaFallback!);
+            return;
+        }
+
+        using var lease = leaseFeature.Lease();
+        var canvas = lease.SkCanvas;
+
+        canvas.Save();
+
+        canvas.Clear(SKColors.White);
+        
+        var rect = new SKRect(10f, 10f, 50f, 50f);
+        var paint = new SKPaint
+        {
+            IsStroke = false,
+            IsAntialias = true,
+            Color = SKColors.SeaGreen
+        };
+        canvas.DrawRoundRect(new SKRoundRect(rect, 10f), paint);
+        
+        canvas.Restore();
+    }
+
+    // TODO: Implement these properly
+    public bool HitTest(Point p) => true;
+    public bool Equals(ICustomDrawOperation? other) => false;
+    public void Dispose()
+    {
+    }
+}
--- Fenet.App/MainWindow.axaml.cs
+++ Fenet.App/MainWindow.axaml.cs
@@ -1,11 +1,63 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
 
 namespace Fenet.App;
 
 public partial class MainWindow : Window
 {
+    private readonly TextBox _addressBar;
+    private readonly Button _goButton;
+
+    private readonly BrowserCanvas _browserCanvas = new();
+    
     public MainWindow()
     {
         InitializeComponent();
+
+        _addressBar = new TextBox
+        {
+            Watermark = "Enter URL",
+            Margin = new Thickness(3d),
+        };
+
+        _goButton = new Button
+        {
+            Content = "Go"
+        };
+        _goButton.Click += GoButton_Click;
+
+        var topPanel = new StackPanel
+        {
+            Orientation = Orientation.Horizontal,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            Children =
+            {
+                _addressBar,
+                _goButton,
+            }
+        };
+        
+        DockPanel.SetDock(topPanel, Dock.Top);
+
+        Content = new DockPanel
+        {
+            Children =
+            {
+                topPanel,
+                _browserCanvas
+            }
+        };
+    }
+
+    private void GoButton_Click(object? sender, RoutedEventArgs e)
+    {
+        if (_addressBar.Text == null)
+        {
+            return;
+        }
+            
+        _browserCanvas.Navigate(_addressBar.Text);
     }
 }
