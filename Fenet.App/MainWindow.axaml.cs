a40a49db5cd1879e204319c8b1a8b81a254fda34
Resongeo <s.benedek2003@gmail.com>
2025-06-21 11:40:23 +0200

    build: Add Fenet.App desktop application project

---
+++ Fenet.App/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Fenet.App;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
--- Fenet.Test/Program.cs
+++ Fenet.Test/Program.cs
@@ -3,16 +3,17 @@ using Fenet.Html;
 
 const string testInput =
 """
- <!DOCTYPE html>
- <html>
-     <div class='flex flex-row' id="container">
-     <!--This is a comment,} the parser ignores it-->
-         <!-This is an incorrect comment, the parser should ignore it-->
-         <h1>Hellope</h1>
-         <p disabled>Lorem ipsum</p>
-     </div>
- </html>
- """;
+<!DOCTYPE html>
+<html>
+    <div class='flex flex-row' id="container">
+    <!--This is a comment,} the parser ignores it-->
+        <!-This is an incorrect comment, the parser should ignore it-->
+        <h1>Hellope</h1>
+        <p>Lorem ipsum</p>
+        <button disabled>Disabled</button>
+    </div>
+</html>
+""";
 
 var browser = new BrowserEngine();
 var dom = browser.TestParse(testInput);
